839d7f277e089c0b8289308cfdc0899ffe7a0fdb
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-06-28 15:23:37 +0000

    accepts_nested_attributes_forメゾットの設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,6 @@
 class Property < ApplicationRecord
- has_many :nearest_station
+ has_many :nearest_station, dependent: :destroy, inverse_of: :property
+ #アソシエーション設定,親モデルのデータを削除した時に子モデルも一緒に削除される。
+ accepts_nested_attributes_for :nearest_stations, allow_destroy: true, reject_if: :all_blank
+#reject_all_blank関数でtrueが出たパラメータは、送信データから除外させていく。
 end
