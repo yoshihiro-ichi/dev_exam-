93b10c79d117746e98a263181b7a0ac5adfcedd6
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-06-28 16:57:16 +0000

    viewとcontrollerの設定

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,64 +1,65 @@
 class PropertiesController < ApplicationController
-  before_action :set_property, only: %i[ show edit update destroy ]
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
 
-  # GET /properties or /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1 or /properties/1.json
-  def show
-  end
-
-  # GET /properties/new
   def new
     @property = Property.new
+    2.times { @property.nearest_stations.build }
   end
 
-  # GET /properties/1/edit
-  def edit
-  end
-
-  # POST /properties or /properties.json
   def create
     @property = Property.new(property_params)
-
-    respond_to do |format|
-      if @property.save
-        format.html { redirect_to @property, notice: "Property was successfully created." }
-        # format.json { render :index, status: :created, location: @property }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        # format.json { render json: @property.errors, status: :unprocessable_entity }
-      end
+    if @property.save
+      redirect_to properties_path, notice: "物件を登録しました！"
+    else
+    render :new
     end
   end
 
-  # PATCH/PUT /properties/1 or /properties/1.json
-  def update
-    respond_to do |format|
-      if @property.update(property_params)
-        format.html { redirect_to @property, notice: "物件情報を更新しました" }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
+  def show
+    @nearest_stations = @property.nearest_stations
+  end
 
-      end
+  def edit
+    @property.nearest_stations.build
+  end
+
+  def update
+    if @property.update(property_params)
+      redirect_to properties_path, notice: "物件を編集しました！"
+    else
+      render :edit
     end
   end
 
   def destroy
     @property.destroy
-    respond_to do |format|
-      format.html { redirect_to properties_url, notice: "Property was successfully destroyed." }
-    end
+    redirect_to properties_path
   end
 
   private
-    def set_property
-      @property = Property.find(params[:id])
-    end
+  def property_params
+    params.require(:property).permit(
+      :property_name,
+      :rent,
+      :street_address,
+      :age,
+      :note,
+      nearest_stations_attributes: [
+        :route_name,
+        :station_name,
+        :time,
+        :property_id,
+        :id,
+        :_destroy,
+      ],
+    )
+  end
 
-    def property_params
-      params.require(:property).permit(:property_name, :rent, :address, :building_age, :note)
-    end
+  def set_property
+    @property = Property.find(params[:id])
+  end
 end
